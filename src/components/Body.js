8d28f94a11ead1e4491539df139e92293e768024
Sanskar Ram <sanskarram992@gmail.com>
2023-11-17 20:06:09 +0530

    changes done

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,40 +1,65 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import resList, { simp } from "../utils/MockData";
 import RestaurantCard from "./RestaurantCard";
-
+import Shimmer from "./Shimmer";
 const Body = () => 
 {
-  const[restList,setRestList] = useState(resList);
-  return(
+   const[restList,setRestList] = useState([]);
+   const[filteredRestList,setFilteredRestList] = useState([]);
+   const[searchText,setSearchText] = useState("");
+   
+
+  const getData = async () => {
+    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.8040881&lng=86.3085152&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+    const jsonData = await data.json();
+    if(jsonData?.data?.cards[3]?.card?.card?.gridElements?.infoWithStyle?.restaurants === undefined)
+    {
+    setRestList([]);
+    setFilteredRestList([]);
+    }
+    else
+    {
+    setRestList(jsonData?.data?.cards[3]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    setFilteredRestList(jsonData?.data?.cards[3]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    }
+  }
+
+   useEffect(()=>{getData()},[]);
+
+   return restList.length === 0 ? <Shimmer/> : (
     <div className="body">
-    <div className="search-filter">
-     <label>Search</label>
-     <input placeholder="search"></input>
-     <div className="filter">
+      <div className="filter">
+      <div className="search">
+     <input className="search-box" type="text" placeholder="search" value={searchText} onChange={(e)=>{setSearchText(e.target.value)}}></input>
+     <button onClick={()=>{
+         setFilteredRestList(restList.filter((res) => res?.info?.name.toLowerCase().includes(searchText.toLowerCase())));
+     }}>Search</button>
+     </div>
+     <div className="top-rating">
       <button className="filter-button" onClick={() => {
-        setRestList(resList.filter((res) => res.info.avgRating >= 4));
+        setFilteredRestList(restList.filter((res) => res.info.avgRating >= 4));
       }}>Top Category Restaurant</button>
      </div>
-     <div>
-        <label>Rating</label>
-        <select>
-         <option>&#9733;&#9733;&#9733;&#9733;&#9733;</option>
-         <option>&#9733;&#9733;&#9733;&#9733;</option>
-         <option>&#9733;&#9733;&#9733;</option>
-         <option>&#9733;&#9733;</option>
-         <option>&#9733;</option>
+     <div className="rating-filter">
+        <button>Rating</button>
+        <select defaultValue={2.5} onChange={(e)=>{
+          setFilteredRestList(restList.filter((res) => res.info.avgRating > e.target.value));
+        }}>
+         <option value={4.5}>rating 4.5+</option>
+         <option value={4.0}>rating 4.0+</option>
+         <option value={3.5}>rating 3.5+</option>
+         <option value={3.0}>rating 3.0+</option>
+         <option value={2.5}>rating 2.5+</option>
         </select>
-        
      </div>
      <div className="showing">
-        Showing {restList.length} of {resList.length}
+        Showing {filteredRestList.length} of {restList.length}
      </div>
     </div>
     <div className="res-container">
      {
-        restList.map((res) => <RestaurantCard key={res.info.id} resData={res}/>)
+        filteredRestList.map((res) => <RestaurantCard key={res.info.id} resData={res}/>)
      }   
-
     </div>
     </div>
    );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,10 @@
+import { useState } from "react";
 import { LOGO_URL } from "../utils/Constants";
 
 
 const Header = () => 
 {
+   const [btnText,setbtnText] = useState("Login");
    return(
     <div className="header">
     <div className="logo-container">
@@ -14,6 +16,7 @@ const Header = () =>
        <li>About</li>
        <li>Contact</li>
        <li>Cart</li>
+       <li><button className="login" onClick={()=>{btnText === "Login"?setbtnText("LogOut"):setbtnText("Login")}}>{btnText}</button></li>
      </ul>
     </div>
     </div>
