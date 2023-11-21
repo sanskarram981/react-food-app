332146142f81ca98fda967483016fe8c49fcd54e
Sanskar Ram <sanskarram992@gmail.com>
2023-11-21 14:49:49 +0530

    changes done

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,19 +1,13 @@
 import { useEffect,useState } from "react";
 import Shimmer from "./Shimmer";
 import { useParams } from "react-router-dom";
+import useRestaurantMenu from "../utils/useRestaurantMenu";
 
-const RestaurantMenu = (Id) => {
-    const [resMenu,setResMenu] = useState(null);
+const RestaurantMenu = () => {
     const {resId} = useParams();
-    useEffect(()=>{getData()},[]);
+    const resMenu = useRestaurantMenu(resId);
 
-    const getData = async () => {
-        const URL = `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9360036&lng=77.6808131&restaurantId=${resId}&catalog_qa=undefined&submitAction=ENTER`;
-        const jsonData = await fetch(URL);
-        const myData = await jsonData.json();
-        setResMenu(myData);
-    }
-
+    console.log("RestaurantMenu-render");
     if(resMenu === null)
     return <Shimmer/>
 
---
+++ src/utils/useRestaurantMenu.js
@@ -0,0 +1,18 @@
+import {useState,useEffect} from "react";
+
+const useRestaurantMenu = (resId) =>
+{
+    const [resMenu,setResMenu] = useState(null);
+    useEffect(()=>{getData();console.log("useRestaurantMenu-useEffect")},[]);
+
+    const getData = async () => {
+        const URL = `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9360036&lng=77.6808131&restaurantId=${resId}&catalog_qa=undefined&submitAction=ENTER`;
+        const jsonData = await fetch(URL);
+        const myData = jsonData.status === 200 ? await jsonData.json() : [];
+        setResMenu(myData);
+    }
+
+    return resMenu;
+}
+
+export default useRestaurantMenu;
