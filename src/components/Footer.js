2693b63fddd59fac6ccb8a0d55beb96409fba1c0
Sanskar Ram <sanskarram992@gmail.com>
2023-11-22 11:41:14 +0530

    changes done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ const Footer = () =>
 {
    return(
     <div className="footer">
-    <h1>---------------नटखट बिहार-------------</h1>
+    <h1 className="text-center bg-gray-500 rounded-lg mx-96">---------------नटखट बिहार-------------</h1>
     </div>
    );
 };
